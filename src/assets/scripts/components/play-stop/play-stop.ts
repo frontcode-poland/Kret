d09687ed3e38e7f27e61b9aa41c0fd05e03d6ff5
Dawid Lewandowski <dawid.lewandowski13@gmail.com>
2018-12-09 20:34:53 +0100

    poczatki - link yt i obiekt parmertow

--- src/assets/scripts/components/play-stop/play-stop.ts
+++ src/assets/scripts/components/play-stop/play-stop.ts
@@ -1,3 +1,8 @@
 export default function () {
     console.log('Play and Stop component');
+
+    var timeout = document.getElementById("timeout").value.trim();
+
+    //if (timeout > 0) alert("alert");document.getElementById("wynik").innerHTML=timeout;
+    //timeout.addEventListener("changes",alert("Zmieniono parametr"))
 };
